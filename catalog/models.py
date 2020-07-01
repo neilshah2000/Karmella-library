50c37aba64cda4a65faff14751b456001c1360b6
Your Name <you@example.com>
2020-07-01 18:57:51 +0100

    created library app

---
+++ catalog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+from .myModels.Author import Author
+from .myModels.Book import Book
+from .myModels.BookInstance import BookInstance
+from .myModels.Genre import Genre
