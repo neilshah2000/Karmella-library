e6a511db8792ef003ffa4b72ad9f376254ed4f40
Your Name <you@example.com>
2020-07-13 16:46:00 +0100

    added static folder

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
