296470a565c5188162299826677a5a1d2de0bbf2
Your Name <you@example.com>
2020-07-13 18:16:34 +0100

    added front end static files

---
+++ catalog/static/precache-manifest.aa17a08a292dff765b4b5077998024de.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6878433111e40bc693e249107f1844d2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c14dfe9e078f1617b98",
+    "url": "./static/css/0.c04d175b.chunk.css"
+  },
+  {
+    "revision": "e286f54282f03c444b3c",
+    "url": "./static/css/main.20cf3e90.chunk.css"
+  },
+  {
+    "revision": "9c14dfe9e078f1617b98",
+    "url": "./static/js/0.9f8ee4b4.chunk.js"
+  },
+  {
+    "revision": "6a158f341dc1e3049568872cfa992e48",
+    "url": "./static/js/0.9f8ee4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "571b23c0d7b82c969fa9",
+    "url": "./static/js/10.4ef126e6.chunk.js"
+  },
+  {
+    "revision": "5274638d4da76724b725",
+    "url": "./static/js/11.63ca9f2e.chunk.js"
+  },
+  {
+    "revision": "31930cf67296cad68ace",
+    "url": "./static/js/3.b974dea5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/3.b974dea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76110a34b2888f792f07",
+    "url": "./static/js/4.22d5540a.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "./static/js/4.22d5540a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb76dc886cfe0d4e3b5",
+    "url": "./static/js/5.9989af0e.chunk.js"
+  },
+  {
+    "revision": "a16bc6da978d0d0e93239b2ef9f7d540",
+    "url": "./static/js/5.9989af0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8bd8db5aff57d50a8d",
+    "url": "./static/js/6.d070289a.chunk.js"
+  },
+  {
+    "revision": "1c44acd9b01e65fc830f",
+    "url": "./static/js/7.8315cacc.chunk.js"
+  },
+  {
+    "revision": "48791b42f65a0246e2cc",
+    "url": "./static/js/8.5a514b29.chunk.js"
+  },
+  {
+    "revision": "a5db90b755bec8875f17",
+    "url": "./static/js/9.13dc51de.chunk.js"
+  },
+  {
+    "revision": "e286f54282f03c444b3c",
+    "url": "./static/js/main.0d3b921d.chunk.js"
+  },
+  {
+    "revision": "d6000e95e1b7d2b804c7",
+    "url": "./static/js/runtime-main.778ed333.js"
+  }
+]);
