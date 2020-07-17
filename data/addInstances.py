1407be18208723c44f9fa60fa620dd0ee3ed7c51
Your Name <you@example.com>
2020-07-17 14:14:58 +0100

    added migrations to release process

--- data/addInstances.py
+++ data/addInstances.py
@@ -4,8 +4,8 @@ import os
 from BookInstanceCreator import BookInstanceCreator
 from helpers import returnBookInstancesFromZoteroJson, addBookInstance
 
-serverUrl = 'http://127.0.0.1:8000/'
-# serverUrl = 'https://blooming-mountain-86004.herokuapp.com/'
+# serverUrl = 'http://127.0.0.1:8000/'
+serverUrl = 'https://blooming-mountain-86004.herokuapp.com/'
 
 zoteroAllBooksFile = '/home/neil/Code/library/locallibrary/data/Karmelako_liburutegia.json'
 
